5506867c1f93b890c27f4afd14531735a0e4e425
BohdanHamulets <bogdan.robota@gmail.com>
2017-11-14 16:16:22 +0200

    added folder for learning importing stuff

---
+++ pryklad_importy/new_temp.py
@@ -0,0 +1,3 @@
+from tumchas import Return_whats_typed_in
+a = 7.025655
+print(Return_whats_typed_in(a))
---
+++ pryklad_importy/tumchas.py
@@ -0,0 +1,4 @@
+def Return_whats_typed_in(bla):
+    return (bla)
+
+#print(Return_whats_typed_in("bla"))
