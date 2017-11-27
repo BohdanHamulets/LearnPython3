e7ae8390837f76c1c3027f3d767bdaf5caf1cc47
BohdanHamulets <bogdan.robota@gmail.com>
2017-11-27 14:40:36 +0200

    added html capable email script

--- GMAIL_project/day10.py
+++ GMAIL_project/day10.py
@@ -15,3 +15,17 @@ email_conn.starttls()
 email_conn.login(username, password)
 email_conn.sendmail(from_header, to_list, "Hellow, test")
 email_conn.quit()
+
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException 
+
+naslidyvannja_classy = SMTP(host, port)
+naslidyvannja_classy.ehlo()
+naslidyvannja_classy.starttls()
+try:
+    naslidyvannja_classy.login(username, "Wrong Pass")
+    naslidyvannja_classy.sendmail(from_header, to_list, "Hellow, test")
+except SMTPAuthenticationError:
+    print("SMTPAuthenticationError occured")
+except:
+    print("Some other error occured")
+naslidyvannja_classy.quit()
---
+++ GMAIL_project/html_formated_mail.py
@@ -0,0 +1,45 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+
+host = "smtp.gmail.com"
+port = 587
+username = "chillimillitilli@gmail.com"
+password = 945094509450
+from_header = "chillimillitilli@gmail.com"
+to_list = ["chillimillitilli@gmail.com"]
+
+
+email_conn = smtplib.SMTP(host, port)
+email_conn.ehlo()
+email_conn.starttls()
+email_conn.login(username, password)
+try:
+    the_msg = MIMEMultipart("alternative")
+    the_msg['Subject'] = "Hello, there"
+    the_msg["From"] = from_header
+    the_msg["To"] = to_list[0]
+    plain_txt = "This is a body of the message"
+    html_txt= """\
+    <html>
+        <head></head>
+        <body>
+            <p>Hey! I just met you, and zis is crazy!<br>
+            But here's my number <b>380937088188</b>.
+            Made by <a href='http://google.com'>GOOGLE</a>.
+            </p>
+        </body>
+    </html>
+    """
+    part1 = MIMEText(plain_txt, 'plain')
+    part2 = MIMEText(html_txt, "html")
+    the_msg.attach(part1)
+    the_msg.attach(part2)
+    email_conn.sendmail(from_header, to_list, the_msg.as_string())
+    email_conn.quit()
+except smtplib.SMTPAuthenticationError:
+    print("SMTPAuthenticationError occured")
+except:
+    print("Some other error occured")
