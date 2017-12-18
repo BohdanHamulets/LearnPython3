153813d63b27d242fa8f34af512596be41bb49ed
BohdanHamulets <bogdan.robota@gmail.com>
2017-12-18 18:36:24 +0200

    removed not used global var

--- recurs_ran_choise.py
+++ recurs_ran_choise.py
@@ -3,14 +3,12 @@ import random
 list_for_random = ["go_on", "don't be scared", "continue", "keep going", "proceed", "go again", "stop",]
 
 def rekyrsia(list_of_actions):
-    global x
     x = 0
     what_was_it = random.choice(list_of_actions)
     if  what_was_it == "stop":
-        print("choise was to stop")
+        print("choice was to stop")
     else:
         x =+1
-        print(x)
         print('"Choise was -', what_was_it,'"')
         rekyrsia(list_of_actions)
 
